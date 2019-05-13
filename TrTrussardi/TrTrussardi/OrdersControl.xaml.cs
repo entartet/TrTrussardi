c93b900a7b75a2d18a0594c532ace380a2ac017e
entartet <ilmir.sultangulov@mail.ru>
2019-05-13 22:09:45 +0300

    Finally!!!

--- TrTrussardi/TrTrussardi/OrdersControl.xaml.cs
+++ TrTrussardi/TrTrussardi/OrdersControl.xaml.cs
@@ -22,14 +22,43 @@ namespace TrTrussardi
         public OrdersControl()
         {
             InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            Ot.SelectedDate = DateTime.Now.AddDays(-7);
+            Do.SelectedDate = DateTime.Now;
             Refresh();
         }
         void Refresh()
         {
             tr = new TrTrussardiEntities();
             tr.Orders.Load();
-            OrdersGrid.ItemsSource = tr.Orders.Local.ToBindingList();
-            
+            var ord = tr.Orders.AsQueryable();
+            bool status = true;
+            bool canStatus = false;
+            if (comboBox1.SelectedIndex == 0)
+                canStatus = true;
+            if (comboBox1.SelectedIndex == 1)
+                    status = true;
+            if (comboBox1.SelectedIndex == 2)
+                status = false;
+            if(canStatus==false)
+                ord = ord.Where(a => a.Status == status);
+            ord = ord.Where(a => a.Time >= Ot.SelectedDate);
+            ord = ord.Where(a => a.Time <= Do.SelectedDate);
+            int sum = 0;
+            foreach(Orders orders in ord.ToList())
+                sum += (int)orders.AllCost;
+            AllCostText.Text = sum.ToString();
+            OrdersGrid.ItemsSource = ord.ToList();
+        }
+
+        private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void Ot_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
         }
     }
 }

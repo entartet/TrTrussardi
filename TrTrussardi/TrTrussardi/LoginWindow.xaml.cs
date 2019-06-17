d3f73a1ca0637f3f61591d68d7b90d68cc121de8
entartet <ilmir.sultangulov@mail.ru>
2019-06-17 23:37:45 +0300

    1.0

--- TrTrussardi/TrTrussardi/LoginWindow.xaml.cs
+++ TrTrussardi/TrTrussardi/LoginWindow.xaml.cs
@@ -20,46 +20,63 @@ namespace TrTrussardi
         }
         private void SignIn_Click_1(object sender, RoutedEventArgs e)
         {
-            
-            db.Users.Load();
-            if (comboBox1.Text.Trim() != "" && txtPassword.Password.Trim() != "")
+            try
             {
-                var auth_login = db.Users.Where(a => a.Login == (comboBox1.Text.Trim()));
-                auth_login = auth_login.Where(a => a.Password == txtPassword.Password);
-                Users user = auth_login.First();
-                if(user != null)
-                    // успешный вход пароль и логин правильные
-                    if (user.PositionId == 3)
-                    {
-                        // Открываем форму администратора
-                        MainWindow main = new MainWindow();
-                        main.Show();
-                        Close();
-                        return;
-                    }
-                    if (user.PositionId == 2)
-                    {
-                        // Открываем форму кассира
-                        MainForUser main = new MainForUser();
-                        main.Show();
-                        Close();
-                        return;
-                    }
-                    if (user.PositionId == 1)
-                    {
-                        // Открываем форму повара
-                        CookWindow main = new CookWindow();
-                        main.Show();
-                        Close();
-                        return;
-                    }
-                else
+                db.Users.Load();
+                if (comboBox1.Text.Trim() != "" && txtPassword.Password.Trim() != "")
                 {
-                    MessageBox.Show("Введены неверные данные");
+                    try
+                    {
+                        var auth_login = db.Users.Where(a => a.Login == (comboBox1.Text.Trim()));
+                        auth_login = auth_login.Where(a => a.Password == txtPassword.Password);
+                        Users user = auth_login.First();
+                        if (user != null)
+                            // успешный вход пароль и логин правильные
+                            if (user.PositionId == 3)
+                            {
+                                // Открываем форму администратора
+                                MainWindow main = new MainWindow();
+                                main.Show();
+                                Close();
+                                return;
+                            }
+                        if (user.PositionId == 2)
+                        {
+                            // Открываем форму кассира
+                            MainForUser main = new MainForUser();
+                            main.Show();
+                            Close();
+                            return;
+                        }
+                        if (user.PositionId == 1)
+                        {
+                            // Открываем форму повара
+                            CookWindow main = new CookWindow();
+                            main.Show();
+                            Close();
+                            return;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Введены неверные данные");
+                        }
+                        db.Dispose();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Введены неверные данные");
+                        return;
+                    }
+
                 }
-                db.Dispose();
+                MessageBox.Show("Необходимо заполнить все поля");
+                return;
             }
-            MessageBox.Show("Необходимо заполнить все поля");
+            catch
+            {
+                MessageBox.Show("не удалось подключиться к базе данных");
+            }
+            
         }
 
         private void LogForm_Closing(object sender, System.ComponentModel.CancelEventArgs e)
